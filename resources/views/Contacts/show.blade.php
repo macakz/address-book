08d55f5c886395ab69262f3a3e10e3ce1c80fd23
Alexander McKerrow <mckerrow4@gmail.com>
2022-03-10 15:47:15 +1300

    css update

--- resources/views/Contacts/show.blade.php
+++ resources/views/Contacts/show.blade.php
@@ -2,52 +2,52 @@
 
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div>
-                <h2>Address Book</h2>
-            </div>
-            <div>
-                <a class="btn btn-primary" href="{{ route('contacts.index') }}" title="Go back">
-                    Contacts
-                </a>
-            </div>
-        </div>
-    </div>
-    
-    <div class="row">
-            <div class="form-group">
-                <strong>Name:</strong>
+
+    <nav class="navbar navbar-dark">
+        <div>
+            <h2 class="text-light">
                 {{ $contact->name }}
+            </h2>
+        </div>
+        <span class="navbar-text">
+            <a class="btn btn-primary" href="{{ route('contacts.index') }}" title="Go back">
+                <i class="fas fa-backward "></i>
+            </a>
+        </span>
+    </nav>  
+
+    <div class="row text-light pt-4">
+            <div class="form-group">
+                <strong><u>Email:</u></strong>
+                <br>
+                {{ $contact->email }}
             </div>
             <div class="form-group">
-                <strong>Email:</strong>
-                {{ $contact->introduction }}
-            </div>
-            <div class="form-group">
-                <strong>Phone Number:</strong>
+                <strong><u>Phone Number:</u></strong>
+                <br>
                 {{ $contact->phoneNumber }}
             </div>
             <div class="form-group">
-                <strong>Date of Birth:</strong>
+                <strong><u>Date of Birth:</u></strong>
+                <br>
                 {{ $contact->dateOfBirth }}
             </div>
             <div class="form-group">
-                <strong>Physical Address:</strong>
+                <strong><u>Physical Address:</u></strong>
+                <br>
                 {{ $contact->physicalAddress }}
             </div>
             <div class="form-group">
-                <strong>Date Created:</strong>
+                <strong><u>Date Created:</u></strong>
+                <br>
                 {{ date_format($contact->created_at, 'jS M Y') }}
             </div>
 
         <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
             @csrf
             @method('DELETE')
-
-            <button type="submit" title="delete" style="border: none; background-color:transparent;">
+            <button class="pt-3" type="submit" title="delete" style="border: none; background-color:transparent;">
                 <i class="fas fa-trash fa-lg text-danger"></i>
-
             </button>
         </form>
     </div>
